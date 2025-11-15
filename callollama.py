6ee2d2759babfcdab1dd34f32e4d5b3fde88bb54
Neha Harisham Vishwakarma <nvishwakar2006@gmail.com>
2025-11-16 00:08:23 +0530

    Add files via upload

---
+++ callollama.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def callOLLAMA(user_message):
+    #try:
+        url="http://localhost:11434/api/generate"
+        payload={
+            "model":"phi3",
+            "prompt":user_message,
+            "stream":False
+        }
+        response =requests.post(
+            url,
+            headers={"Content-Type":"application/json"},
+            data=json.dumps(payload),
+            timeout=120
+        )
+        if response.status_code==200:
+            result=response.json()
+            bot_response=result.get("response", "Sorry I am unable to help you")
+            return bot_response.strip()
